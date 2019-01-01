1e666a7af109f4c855c02bb65b98675997a2f857
hhpr98 <39483775+hhpr98@users.noreply.github.com>
2019-01-02 01:28:35 +0700

    Kết nối CSDL cho chức năng tìm kiếm + sửa lỗi Hóa đơn

--- QuanLiNhaSach_N8/BookModel.Designer.cs
+++ QuanLiNhaSach_N8/BookModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\LTWindows\C#\QuanLiNhaSach_N8\QuanLiNhaSach_N8\BookModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\nguyenhuuhoa\Desktop\QuanLiNhaSach_N8\QuanLiNhaSach_N8\BookModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
--- QuanLiNhaSach_N8/Find.xaml.cs
+++ QuanLiNhaSach_N8/Find.xaml.cs
@@ -26,62 +26,174 @@ namespace QuanLiNhaSach_N8
 
         //public static List<Book> listbook = new List<Book>();
 
-        public static bool[] check = new bool[3] { false, false, false };
-
-        /// <summary>
-        /// load data when windows start
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
-            //listbook.Add(new Book { name = "Nhập môn lập trình", Author = "KHTN", Year = 2008 });
-            //listbook.Add(new Book { name = "Kĩ thuật lập trình", Author = "KHTN", Year = 2008 });
-            //listbook.Add(new Book { name = "Tấm cám", Author = "Kim Đồng", Year = 2003 });
-        }
+        public string check = "000";
 
         private void cbName_Checked(object sender, RoutedEventArgs e)
         {
-            check[0] = true;
-            MessageBox.Show("Box Name checked");
+            check = check.Remove(0, 1);
+            check = check.Insert(0, "1");
+            //MessageBox.Show("Box Name checked");
+            //MessageBox.Show(check);
         }
 
         private void cbAuthor_Checked(object sender, RoutedEventArgs e)
         {
-            check[1] = true;
-            MessageBox.Show("Box Author checked");
+            check = check.Remove(1, 1);
+            check = check.Insert(1, "1");
+            //MessageBox.Show("Box Author checked");
+            //MessageBox.Show(check);
         }
 
         private void cbYear_Checked(object sender, RoutedEventArgs e)
         {
-            check[2] = true;
-            MessageBox.Show("Box Year checked");
-        }
-
-        private void cbYear_Unchecked(object sender, RoutedEventArgs e)
-        {
-            check[2] = false;
-            MessageBox.Show("Box Year unchecked");
+            check = check.Remove(2, 1);
+            check = check.Insert(2, "1");
+            //MessageBox.Show("Box Year checked");
+            //MessageBox.Show(check);
         }
 
         private void cbName_Unchecked(object sender, RoutedEventArgs e)
         {
-            check[0] = false;
-            MessageBox.Show("Box Name unchecked");
+            check = check.Remove(0, 1);
+            check = check.Insert(0, "0");
+            //MessageBox.Show("Box Name unchecked");
+            //MessageBox.Show(check);
         }
 
         private void cbAuthor_Unchecked(object sender, RoutedEventArgs e)
         {
-            check[1] = false;
-            MessageBox.Show("Box Author unchecked");
+            check = check.Remove(1, 1);
+            check = check.Insert(1, "0");
+            //MessageBox.Show("Box Author unchecked");
+            //MessageBox.Show(check);
+        }
+
+        private void cbYear_Unchecked(object sender, RoutedEventArgs e)
+        {
+            check = check.Remove(2, 1);
+            check = check.Insert(2, "0");
+            //MessageBox.Show("Box Year unchecked");
+            //MessageBox.Show(check);
         }
 
         private void btnSearch_Click(object sender, RoutedEventArgs e)
         {
-            var windows = new Result();
-            this.Hide();
-            windows.Show();
+            if (check=="000") // Không tick vào checkbox nào cả
+            {
+                var img = MessageBoxImage.Error;
+                var btn = MessageBoxButton.OK;
+                var msg = "Vui lòng chọn ít nhất 1 thuộc tính tìm kiếm!";
+                MessageBox.Show(msg, "Thông báo", btn, img);
+            }
+            else
+            {
+                if (check=="100")
+                {
+                    check = check.Insert(3,"+");
+                    check = check.Insert(4,txtName.Text);
+                }
+
+                if (check=="010")
+                {
+                    check = check.Insert(3, "+");
+                    check = check.Insert(4, txtAuthor.Text);
+                }
+
+                if (check == "001")
+                {
+                    check = check.Insert(3, "+");
+                    check = check.Insert(4, txtYear.Text);
+                }
+
+                if (check == "110")
+                {
+                    check = check.Insert(check.Count(), "+");
+                    check = check.Insert(check.Count(), txtName.Text);
+                    check = check.Insert(check.Count(), "+");
+                    check = check.Insert(check.Count(), txtAuthor.Text);
+                }
+
+                if (check == "101")
+                {
+                    check = check.Insert(check.Count(), "+");
+                    check = check.Insert(check.Count(), txtName.Text);
+                    check = check.Insert(check.Count(), "+");
+                    check = check.Insert(check.Count(), txtYear.Text);
+                }
+
+                if (check == "011")
+                {
+                    check = check.Insert(check.Count(), "+");
+                    check = check.Insert(check.Count(), txtAuthor.Text);
+                    check = check.Insert(check.Count(), "+");
+                    check = check.Insert(check.Count(), txtYear.Text);
+                }
+
+                if (check == "111")
+                {
+                    check = check.Insert(check.Count(), "+");
+                    check = check.Insert(check.Count(), txtName.Text);
+                    check = check.Insert(check.Count(), "+");
+                    check = check.Insert(check.Count(), txtAuthor.Text);
+                    check = check.Insert(check.Count(), "+");
+                    check = check.Insert(check.Count(), txtYear.Text);
+                }
+
+                var windows = new Result();
+                windows.Sender(check);
+                windows.Show();
+                this.Close();
+            }
+        }
+
+        private void BtnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TxtName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (check[0]=='0')
+            {
+                var img = MessageBoxImage.Error;
+                var btn = MessageBoxButton.OK;
+                var msg = "Vui lòng tick vào ô bên cạnh trước khi gõ vào đây!";
+                MessageBox.Show(msg, "Thông báo", btn, img);
+            }
+            else
+            {
+                txtName.Text = "";
+            }
+        }
+
+        private void TxtAuthor_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (check[1] == '0')
+            {
+                var img = MessageBoxImage.Error;
+                var btn = MessageBoxButton.OK;
+                var msg = "Vui lòng tick vào ô bên cạnh trước khi gõ vào đây!";
+                MessageBox.Show(msg, "Thông báo", btn, img);
+            }
+            else
+            {
+                txtAuthor.Text = "";
+            }
+        }
+
+        private void TxtYear_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (check[2] == '0')
+            {
+                var img = MessageBoxImage.Error;
+                var btn = MessageBoxButton.OK;
+                var msg = "Vui lòng tick vào ô bên cạnh trước khi gõ vào đây!";
+                MessageBox.Show(msg, "Thông báo", btn, img);
+            }
+            else
+            {
+                txtYear.Text = "";
+            }
         }
     }
 }
